2d96f2502b00b609d09404910f486390c93139fa
Henry-Tourraine <henry.tourraine@outlook.fr>
2023-03-23 08:42:48 +0100

    test webhook 1

--- API.js
+++ API.js
@@ -102,6 +102,7 @@ app.post('/', async function (req, res) {
    console.log("responding")
    console.log(response);
   res.json({...response});
+  
 })
 
 app.post('/preview', async function (req, res) {
